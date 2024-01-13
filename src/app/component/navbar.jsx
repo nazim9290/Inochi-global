8f6880110b856ac3bf80a9ce127ce697b7831c4a
arefin5 <arefintalukder5@gmail.com>
2024-01-14 00:24:34 +0600

    fix navigation issue

--- src/app/component/navbar.jsx
+++ src/app/component/navbar.jsx
@@ -1,15 +1,14 @@
-// Corrected import statement for useEffect
-"use client";
+"use client"
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from "../../../public/imgages/LOGO.png";
-import { Navbar as BootstrapNavbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Navbar as BootstrapNavbar, Nav, Container } from 'react-bootstrap';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 const Navbar = () => {
   const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
 
@@ -18,60 +17,44 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    // Add an event listener to handle scroll and add a sticky class
     const handleScroll = () => {
       const navbar = document.getElementById('navbar2');
       if (navbar) {
         if (window.scrollY > 0) {
-          navbar.classList.add('sticky');
+          navbar.classList.add('fixed-top');
         } else {
-          navbar.classList.remove('sticky');
+          navbar.classList.remove('fixed-top');
         }
       }
     };
 
-    // Attach the event listener when the component mounts
     window.addEventListener('scroll', handleScroll);
 
-    // Remove the event listener when the component unmounts
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
-    <div>
+    <div className="w-full bg-base-100 shadow">
       <div>
-        <nav className="navbar">
+        <nav className="navbar w-full bg-base-100">
           <div className="wrapper">
             <BootstrapNavbar bg="light" expand="lg" className="navbar navbar-expand-lg">
               <Container fluid>
-                <a href=" https://www.facebook.com/inochi.education" >
-                <FacebookRoundedIcon />
-                </a>
-                <a href="#"  >
-                  <InstagramIcon />
-                </a>
-
-               <a href="https://www.youtube.com/@inochiGlobalEducation" >
-                  <YouTubeIcon />
-                </a>
-                <a href="https://www.linkedin.com/company/inochi-"  >
-                  <LinkedInIcon  />
-                </a>
-                <BootstrapNavbar.Toggle aria-controls="navbarNav" onClick={handleNavbarToggle} />
-                <BootstrapNavbar.Collapse id="navbarNav">
+                
+                
                   <Nav className="navbar-nav mx-auto">
                     <Nav.Link href="#"><b>call</b></Nav.Link>
                     <Nav.Link href="#"><b>WhatsApp</b></Nav.Link>
-                    <Nav.Link href="seminar.html"><b>Fix Online Seminar</b></Nav.Link>
+                    <Nav.Link href="#"><b>Fix Online Seminar</b></Nav.Link>
                   </Nav>
-                </BootstrapNavbar.Collapse>
+               
               </Container>
             </BootstrapNavbar>
           </div>
         </nav>
-        <nav className="navbar navbar-expand-lg navbar2 sticky-top">
+        <nav className="navbar navbar-expand-lg navbar2" id="navbar2">
           <Container fluid>
             <a className="navbar-brand" href="/">
               <Image src={logo} alt="Bootstrap" width={30} height={24} />
@@ -79,24 +62,25 @@ const Navbar = () => {
             <BootstrapNavbar.Toggle aria-controls="navbarSupportedContent" onClick={handleNavbarToggle} />
             <BootstrapNavbar.Collapse in={isNavbarExpanded}>
               <Nav className="navbar-nav me-auto mb-2 mb-lg-0 navbar-center mx-auto">
-                <Nav.Link href="/"><b>HOME</b></Nav.Link>
+              <Nav.Link href="/"><b>HOME</b></Nav.Link>
                 <Nav.Link href="/study"><b>STUDY-SETTLE-JAPAN</b></Nav.Link>
                 <Nav.Link href="/blog"><b>BLOG</b></Nav.Link>
                 <Nav.Link href="/contact"><b>CONTACT US</b></Nav.Link>
                 <Nav.Link href="/dashbord"><b>DASHBOARD</b></Nav.Link>
                 <Nav.Link href="/register"><b>REGISTER</b></Nav.Link>
-                <Nav.Link href="/guest-register"><b>GUEST REGISTER</b></Nav.Link>
+                {/* <Nav.Link href="/guest-register"><b>GUEST REGISTER</b></Nav.Link> */}
                 <Nav.Link href="/login"><b>LOGIN</b></Nav.Link>
               </Nav>
               <form className="d-flex" role="search">
-                <button type="button" className="btn btn-primary me-2" style={{ width: '100%', backgroundColor: '#4f46e5' }}>Join Free</button>
+              <button type="button" className="btn btn-primary me-2" style={{ width: '100%', backgroundColor: '#4f46e5' }}>Join Free</button>
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              
               </form>
             </BootstrapNavbar.Collapse>
           </Container>
         </nav>
-        <>
-        </>
+        {/* The rest of your component */}
+        {/* ... */}
       </div>
     </div>
   );
