5731de619272b95563a5059e6ba1f98749e05657
arefin5 <arefintalukder5@gmail.com>
2024-01-17 03:51:54 +0600

    fixed contact page issue

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,43 +6,15 @@ import Subsscribe from '../component/subscribe';
 import Fotter from '../component/Foter';
 import TopBanner from '../component/TopBanner';
 import ContactForm from '../component/ContactForm';
+import MainBanner from '../component/MainBanner';
 
 const Contact = () => {
-  const [id, setId] = useState(null); // Declare id state
-
-  const [contacpages, setContacPages] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('api/conctact-page');
-        const firstContacPage = response.data.contacpages[0];
-        if (firstContacPage) {
-          let fetchedId = firstContacPage.image.public_id;
-          console.log(fetchedId);
-          setId(fetchedId); // Set the id state
-          setContacPages(response.data.contacpages);
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
 
   return (
     <div>
       <NavBar />
-      {id == null ? (
-        <>
-          Loading ...
-        </>
-      ) : (
-        <>
-          <TopBanner id={id} title="Contact" />
-        </>
-      )}
+      <MainBanner title="Contact Page" />
 
       <ContactForm />
 
