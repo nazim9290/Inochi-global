d16cfd88eabc0b00df0e52ab1f080a75d31d491e
arefin5 <arefintalukder5@gmail.com>
2024-03-23 20:40:25 +0600

    Add dropdown menu

--- src/app/dashbord/page.jsx
+++ src/app/dashbord/page.jsx
@@ -19,13 +19,14 @@
 // export default Dashbord;
 "use client"
 import SideNavbar from "@/app/component/SideNavbar";
+import BasicExample from "@/app/component/SideDropNav"
 import Foter from "@/app/component/Foter";
 const Dashbord = () => {
   return (
     <>
-        <div className="d-flex">
-          <SideNavbar />
-          <div className="w-75" style={{ marginLeft: 'auto' }}>
+        <div className="">
+          <BasicExample />
+          <div className="" style={{ marginLeft: 'auto' }}>
             <p>Home page for test ......</p>
           </div>
         </div>
