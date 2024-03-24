e8f769a8de54cbbe02308b37dee00ba968105b49
arefin5 <arefintalukder5@gmail.com>
2024-03-24 16:22:48 +0600

    add deshbord home page

--- src/app/component/SideNavbar.jsx
+++ src/app/component/SideNavbar.jsx
@@ -172,11 +172,11 @@ function SideNavbar() {
           <h6 className=' text-center'>{name}</h6>
           <p className="text-center">Dhaka, Banglaseh</p>
         </Dropdown.Item>
-        {/* <Dropdown.Item>
+        <Dropdown.Item>
         <div className={`sidebar-item ${isActive('/dashbord') ? 'active' : ''}`}>
             <Link href="/dashbord">Home</Link>
           </div>
-        </Dropdown.Item> */}
+        </Dropdown.Item>
 
     
         <Dropdown.Item>
--- src/app/dashbord/page.jsx
+++ src/app/dashbord/page.jsx
@@ -21,15 +21,39 @@
 import SideNavbar from "@/app/component/SideNavbar";
 import BasicExample from "@/app/component/SideDropNav"
 import Foter from "@/app/component/Foter";
+import React, { useEffect, useContext, useState } from 'react'
+import Link from 'next/link';
+import { useRouter, usePathname } from "next/navigation";
+import logo from "../../../public/imgages/LOGO.png"
+import Image from 'next/image';
+import { UserContext } from "../../context";
+
 const Dashbord = () => {
+  const router = useRouter();
+
+
+const pathname = usePathname();
+const [state, setState] = useContext(UserContext);
+const [name, setName] = useState("");
+const [image, setImage] = useState({});
+useEffect(() => {
+    if (state && state.user) {
+      setName(state.user.name)
+      setImage(state.user.image)
+
+    } else {
+      router.push("/login")
+    }
+  }, [state, router]);
+  const isActive = (path) => {
+    return pathname === path;
+  };
   return (
     <>
         <div className="">
           <SideNavbar />
           <div className="" style={{ marginLeft: 'auto' }}>
-            <p>
-              
-            </p>
+            <h6 className=' text-center'>Hello {name}</h6>
           </div>
         </div>
         <div className="d-flex">
