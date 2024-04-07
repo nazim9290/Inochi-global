6a1f8230a79c6f0b63b35f9a451ea77f059333cc
arefin5 <arefintalukder5@gmail.com>
2024-04-07 18:33:18 +0600

    change subscriber desigine

--- src/app/component/subscribe.jsx
+++ src/app/component/subscribe.jsx
@@ -32,26 +32,25 @@ const Subsscribe = () => {
 
   return (
     <>
-      <div className="mt-5" style={{ backgroundColor: "#92400e" }}>
+      <div className="mt-5" style={{ backgroundColor: "#12b0bc" }}>
         <div className="row">
-          <div className="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 col-xxl-6 me-5 my-5">
+          <div className="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-12 col-xxl-12 me-5 my-5">
             <div className="container text-center">
               <h2>
                 <b style={{ color: "azure" }} className="text-uppercase">
                   subscribe for our blogs
                 </b>
               </h2>
-              <p style={{ color: "azure" }}>
+              <p style={{ color: "azure" ,textAlign:"center !important"}}>
                 Sign up to receive email updates about the course
               </p>
-            </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-xxl-3  me-5 my-5">
+              <div >
             <form onSubmit={handleSubmit}>
-              <div className="input-group  me-5 my-5">
+            
+              <div className="input-group sebscribe">
                 <input
                   type="text"
-                  className="form-control"
+                  className="form-control text-center"
                   placeholder="Type email here"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
@@ -79,6 +78,9 @@ const Subsscribe = () => {
               </div>
             )}
           </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
